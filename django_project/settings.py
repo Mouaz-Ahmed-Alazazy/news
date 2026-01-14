05c5aa6c0e101579006758070874d28a2c6d7b5a
Mouaz-Ahmed-Alazazy <mouazmaama@gmail.com>
2026-01-14 23:34:34 +0200

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # Local
     "accounts",
     "pages",
+    "articles",
 
 ]
 
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Africa/Tripoli"
 
 USE_I18N = True
 
